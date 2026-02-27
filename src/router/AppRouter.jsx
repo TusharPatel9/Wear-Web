908dfb1f7559238fc2b68ac770973a30e3d15bdf
Umang Patel <umangpatel4200@gmail.com>
2026-02-27 13:52:29 +0530

    admin and seller sidebar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,17 +4,37 @@ import Register from "../components/Register";
 import UserNavbar from "../components/Customer/UserNavbar";
 import MainLayout from "../components/Customer/MainLayout";
 import HomePage from "../pages/HomePage";
+import SellerSidebar from "../components/Seller/SellerSidebar";
+import SellerLayout from "../components/Seller/SellerLayout";
+import AdminLayout from "../components/Admin/AdminLayout";
+
+const SellerDashboard = () => <div>Seller Dashboard</div>;
+const AdminDashboard = () => <div>Admin Dashboard</div>;
+
 
 const router = createBrowserRouter([
-  { path: "/login", element: <Login /> },
-  { path: "/register", element: <Register /> },
+
   {
     path: "/",
     element: <MainLayout />, // layout route
     children: [
       { index: true, element: <HomePage /> },
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
     ],
   },
+  {
+    path: "/seller", element: <SellerLayout />,
+    children: [
+      { path: "dashboard", element: <SellerDashboard /> },
+    ]
+  },
+  {
+    path: "/admin", element: <AdminLayout />,
+    children: [
+      { path: "dashboard", element: <AdminDashboard /> },
+    ]
+  }
 ]);
 
 const AppRouter = () => {
